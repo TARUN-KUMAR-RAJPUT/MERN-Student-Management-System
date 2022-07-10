40578b291b6676750845be47a92e41b0ac59bb3e
Tarun Kumar Rajput <your>
2022-07-10 13:33:17 +0530

    Files Added

---
+++ React Files/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import PostAPI from './PostAPI';
+// import Practice from './Practice';
+import StudentSQLAPI from './StudentSQLAPI';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Practice /> */}
+
+     <StudentSQLAPI />
+    </div>
+  );
+}
+
+export default App;
